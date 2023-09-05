ceb6b3fd4d8e8ed086f85a6e6888f352924d3fb0
eokinyo <50834185+eokinyo@users.noreply.github.com>
2023-09-05 13:52:37 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,44 @@
+
+public class PrinterApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String creator;
+		creator = "Elly Okinyo";
+		int number1 = 29;
+		int number2 = 7;
+		int multiplication;
+		multiplication = number1 * number2;
+		int addition;
+		addition = number1 + number2;
+		double division;
+		division = number1 / number2;
+		int subtraction;
+		subtraction = number1 - number2;
+			
+		System.out.println("Printer App");
+		System.out.println("This App is made by: ");
+		
+		System.out.println(creator);
+		System.out.print("This app is made by: ");
+		System.out.println(creator);
+		
+		System.out.println("The value of number1 is " + number1);
+		//Number successfully printed!
+		System.out.println("The value of number2 is " + number2);
+		
+		System.out.println("Mathematical Operations");
+		//Multiplication
+		System.out.println(multiplication);
+		//Addition
+		System.out.println(addition);
+		//Division
+		System.out.println(division);
+		//Subtraction
+		System.out.println(subtraction);
+		
+		
+		
+	}
+
+}
